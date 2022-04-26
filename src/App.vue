5481ba01e3a6139e1dfd26a92b1d1a1abff0230c
Eugenio <culurciello@gmail.com>
2022-04-26 08:33:43 -0400

    initial commit
    working simple test

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import BabylonScene from './components/BabylonScene.vue'
+</script>
+
+<template>
+
+  <h1>3D Garage</h1>
+
+  <main>
+    <div class="wrapper">
+      <BabylonScene msg="Scene created!" />
+    </div>
+  </main>
+
+</template>
---
+++ src/components/BabylonScene.vue
@@ -0,0 +1,35 @@
+<template>
+  <canvas ref="renderCanvas" id="renderCanvas"/>
+</template>
+
+<script>
+import { ref, onMounted } from "@vue/runtime-core";
+import { createScene } from "../scenes/MyFirstScene";
+
+export default {
+  name: "BabylonScene",
+  setup() {
+    const renderCanvas = ref(null);
+
+    onMounted(() => {
+      if (renderCanvas.value) {
+        createScene(renderCanvas.value);
+      }
+    });
+
+    return {
+      renderCanvas,
+    };
+  },
+};
+</script>
+
+<style>
+#renderCanvas {
+    width: 100%;
+    height: 70%;
+    position: relative;
+    touch-action: none;
+    z-index: 1;
+}
+</style>
